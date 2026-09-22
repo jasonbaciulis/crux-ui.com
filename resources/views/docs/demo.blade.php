87f838a3c2b52793adf0d361a1c21e5afc60b1b2
Jason Baciulis <jason@remarkable.dev>
2026-09-22 17:46:11 +0700

    feat: ui

--- resources/views/docs/demo.blade.php
+++ resources/views/docs/demo.blade.php
@@ -1,7 +1,9 @@
 {{-- laradocs macro view: <x-demo name="collapsible" /> in markdown renders resources/views/blade/demos/collapsible.blade.php --}}
+@props(['center' => false])
+
 <div
     data-slot="component-preview"
-    class="flex min-h-72 w-full items-center justify-center rounded-xl border border-border bg-background p-10 text-foreground"
+    class="flex min-h-72 w-full justify-center rounded-xl border border-border bg-background p-10 text-foreground {{ $center ? 'items-center' : 'items-start' }}"
 >
     @include("demos.{$name}")
 </div>
